6d64d4e39560347f8ff91f0303e873cf1a389b0e
unknown <danadoni.lima@gmail.com>
2022-09-16 11:51:23 -0300

    Criando as estruturas do projeto

---
+++ src/pages/Movies/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import{View,Text} from 'react-native';
+
+
+export default function Movies(){
+
+return(
+
+
+        <View>
+        
+            <Text> MEUS FILMES</Text>
+        
+        </View>
+
+
+);
+
+
+}
